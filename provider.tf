e60fae8f069563b2d66af9d2bb97369f1aa6f120
DeepikaBarik <38130085+DeepikaBarik@users.noreply.github.com>
2019-04-08 18:50:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "AKIA6QHACOD3RTHR3DHV"
-  secret_key = "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"
+  access_key = "AKIA6QHACOD3575M2YUJ"
+  secret_key = "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"
   region     = "ap-south-1"
 }
